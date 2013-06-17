00c62ab83f14d86e1f81ddf617d5f3a34731567c
vemacs <gentoo@outlook.com>
2013-06-17 11:53:42 -0400

    First commit

---
+++ src/main/java/com/nullblock/vemacs/antifoottrap/AntiFootTrap.java
@@ -0,0 +1,41 @@
+package com.nullblock.vemacs.antifoottrap;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class AntiFootTrap extends JavaPlugin implements Listener {
+
+    public static ConcurrentHashMap<String, Location> triggeredPlayers = new ConcurrentHashMap();
+    public static ConcurrentHashMap<String, Integer> playerTimes = new ConcurrentHashMap();
+
+
+    public void onDisable() {
+    }
+
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+        BukkitTask task = new CheckLocation().runTaskTimer(this, 0, CheckLocation.interval);
+    }
+
+    @EventHandler
+    public void onPlayerDamage(EntityDamageEvent event) {
+        Entity e = event.getEntity();
+        if (e instanceof Player && event instanceof EntityDamageByEntityEvent && ((EntityDamageByEntityEvent) event).getDamager() instanceof Player) {
+            Player p = (Player) e;
+            if (!triggeredPlayers.contains(p.getName())) {
+                triggeredPlayers.put(p.getName(), p.getLocation());
+                playerTimes.put(p.getName(), 0);
+            }
+        }
+    }
+}
---
+++ src/main/java/com/nullblock/vemacs/antifoottrap/CheckLocation.java
@@ -0,0 +1,49 @@
+package com.nullblock.vemacs.antifoottrap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CheckLocation extends BukkitRunnable {
+
+    public static int interval = 2;
+    public float sec = 10;
+
+    public CheckLocation() {
+
+    }
+
+    public void run() {
+        for (ConcurrentHashMap.Entry<String, Location> entry : AntiFootTrap.triggeredPlayers.entrySet()) {
+            Player p = Bukkit.getPlayer(entry.getKey());
+            Location l1 = entry.getValue();
+            AntiFootTrap.triggeredPlayers.put(p.getName(), p.getLocation());
+            if (isSameXZ(l1, p.getLocation())) {
+                AntiFootTrap.playerTimes.put(p.getName(), AntiFootTrap.playerTimes.get(p.getName()) + 1);
+                if (AntiFootTrap.playerTimes.get(p.getName()) >= sec * (20 / interval)) {
+                    p.teleport(p.getWorld().getSpawnLocation());
+                    AntiFootTrap.triggeredPlayers.remove(p.getName());
+                    AntiFootTrap.playerTimes.remove(p.getName());
+                    getPlugin().getLogger().info(p.getName() + " has been TPed to spawn");
+                }
+            }
+        }
+    }
+
+    private boolean isSameXZ(Location loc1, Location loc2) {
+        float range = 1.1F;
+        if (Math.abs(loc1.getX() - loc2.getX()) < (range / 2) && Math.abs(loc1.getZ() - loc2.getZ()) < (range / 2)) {
+            return true;
+        }
+        return false;
+    }
+
+    private Plugin getPlugin() {
+        return Bukkit.getPluginManager().getPlugin("AntiFootTrap");
+    }
+
+}
